68e190b36c644f8dc15acae8e350cb4f79912a8a
gjf7128 <gjf7128@rit.edu>
2024-05-05 20:22:41 -0400

    sockets completed

--- sockets/client.c
+++ sockets/client.c
@@ -5,7 +5,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+#include <time.h>
 
 void error(const char *msg)
 {
@@ -13,68 +14,129 @@ void error(const char *msg)
     exit(0);
 }
 
+// For the connection overhead and speed
+// int main(int argc, char *argv[])
+// {
+//     int sockfd, portno, n;
+//     struct sockaddr_in serv_addr;
+//     struct hostent *server;
+
+//     // char buffer[256] = "0123456789";
+//     if (argc < 3) {
+//        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+//        exit(0);
+//     }
+//     portno = atoi(argv[2]);
+//     // clock_t start = clock();
+
+//     for (int i = 1; i <= 1000; i++) {
+//         char buffer[256] = "0123456789";
+//         sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//         if (sockfd < 0) 
+//             error("ERROR opening socket");
+//         server = gethostbyname(argv[1]);
+//         if (server == NULL) {
+//             fprintf(stderr,"ERROR, no such host\n");
+//             exit(0);
+//         }
+//         int opt = 1;
+//         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+//             perror("setsockopt");
+//             exit(EXIT_FAILURE);
+//         }
+
+//         bzero((char *) &serv_addr, sizeof(serv_addr));
+//         serv_addr.sin_family = AF_INET;
+//         bcopy((char *)server->h_addr, 
+//              (char *)&serv_addr.sin_addr.s_addr,
+//              server->h_length);
+//         serv_addr.sin_port = htons(portno);
+//         if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+//             error("ERROR connecting");
+//         printf("Looping 1000x while sending 10 byte msg...");
+//         // bzero(buffer,256);
+//         // fgets(buffer,255,stdin);
+//         n = write(sockfd,buffer,strlen(buffer));
+//         if (n < 0) 
+//             error("ERROR writing to socket");
+//         // bzero(buffer,256);
+//         memset(buffer, 0, sizeof(buffer));
+//         n = read(sockfd,buffer,255);
+//         if (n < 0) 
+//             error("ERROR reading from socket");
+//         printf("%s\n",buffer);
+//         close(sockfd);
+//         // bzero(buffer, strlen(buffer));
+//         memset(buffer, 0, sizeof(buffer));
+//     }
+
+//     // This timer stuff is hardly accurate when using my phone timer in comparison, so i'm leaving it here,
+//     // commented out
+//     // double difference = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+//     // double differenceOverLoops = difference / 1000;
+//     // printf("Connection time is %f seconds elapsed over %d iterations which equals %f seconds", difference, 1000, differenceOverLoops);
+//     // printf("1000 loops have concluded");
+
+//     return 0;
+// }
+
+// For getting bandwidth
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256] = "0123456789";
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
     portno = atoi(argv[2]);
-    clock_t start = clock();
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+        (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
     for (int i = 1; i <= 1000; i++) {
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) 
-            error("ERROR opening socket");
-        server = gethostbyname(argv[1]);
-        if (server == NULL) {
-            fprintf(stderr,"ERROR, no such host\n");
-            exit(0);
-        }
-        bzero((char *) &serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        bcopy((char *)server->h_addr, 
-             (char *)&serv_addr.sin_addr.s_addr,
-             server->h_length);
-        serv_addr.sin_port = htons(portno);
-        if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-            error("ERROR connecting");
-        printf("Looping 1000x while sending 10 byte msg...");
-        // bzero(buffer,256);
-        // fgets(buffer,255,stdin);
-        n = write(sockfd,buffer,strlen(buffer));
+        char buffer[1401];
+        memset(buffer, 'a', sizeof(buffer) - 1);
+        buffer[sizeof(buffer) - 1] = '\0';
+        printf("Looping 1000x while sending 1400 byte msg...");
+        n = write(sockfd,buffer,sizeof(buffer));
         if (n < 0) 
             error("ERROR writing to socket");
-        bzero(buffer,256);
-        n = read(sockfd,buffer,255);
+        memset(buffer, 0, sizeof(buffer));
+        n = read(sockfd,buffer,sizeof(buffer));
         if (n < 0) 
             error("ERROR reading from socket");
         printf("%s\n",buffer);
-        close(sockfd);
-        bzero(buffer, strlen(buffer));
-        sleep(60);
+        memset(buffer, 0, sizeof(buffer));
     }
-    int difference = start - clock();
-    int differenceOverLoops = difference / 1000;
-    printf("1000 loops have concluded");
-    printf("Connection time is %d elapsed over %d iterations which equals %d seconds", difference, 1000, differenceOverLoops);
-    // Uncomment this code to bring it to it's initial state
-    // if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-    //     error("ERROR connecting");
-    // printf("Please enter the message: ");
-    // bzero(buffer,256);
-    // fgets(buffer,255,stdin);
-    // n = write(sockfd,buffer,strlen(buffer));
-    // if (n < 0) 
-    //      error("ERROR writing to socket");
-    // bzero(buffer,256);
-    // n = read(sockfd,buffer,255);
-    // if (n < 0) 
-    //      error("ERROR reading from socket");
-    // printf("%s\n",buffer);
+    close(sockfd);
+
+    // This timer stuff is hardly accurate when using my phone timer in comparison, so i'm leaving it here,
+    // commented out
+    // double difference = ((double)(clock() - start)) / CLOCKS_PER_SEC;
+    // double differenceOverLoops = difference / 1000;
+    // printf("Connection time is %f seconds elapsed over %d iterations which equals %f seconds", difference, 1000, differenceOverLoops);
+    // printf("1000 loops have concluded");
+
     return 0;
 }
--- sockets/server.c
+++ sockets/server.c
@@ -14,60 +14,120 @@ void error(const char *msg)
     exit(1);
 }
 
+// This code gets connection overhead and speed
+// int main(int argc, char *argv[])
+// {
+//      int sockfd, newsockfd, portno;
+//      socklen_t clilen;
+//      char buffer[256];
+//      struct sockaddr_in serv_addr, cli_addr;
+//      int n;
+//      if (argc < 2) {
+//          fprintf(stderr,"ERROR, no port provided\n");
+//          exit(1);
+//      }
+//      int count = 1;
+//      while (count <= 1000) {
+//          sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//          int opt = 1;
+//          if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+//              perror("setsockopt");
+//              exit(EXIT_FAILURE);
+//          }        
+//          if (sockfd < 0) 
+//              error("ERROR opening socket");
+//          bzero((char *) &serv_addr, sizeof(serv_addr));
+//          portno = atoi(argv[1]);
+//          serv_addr.sin_family = AF_INET;
+//          serv_addr.sin_addr.s_addr = INADDR_ANY;
+//          serv_addr.sin_port = htons(portno);
+//          if (bind(sockfd, (struct sockaddr *) &serv_addr,
+//                   sizeof(serv_addr)) < 0) 
+//                   error("ERROR on binding");
+//          listen(sockfd,5);
+//          clilen = sizeof(cli_addr);
+//          newsockfd = accept(sockfd, 
+//                  (struct sockaddr *) &cli_addr, 
+//                  &clilen);
+//          if (newsockfd < 0) 
+//               error("ERROR on accept");
+//         //  bzero(buffer,256);
+//          memset(buffer, 0, sizeof(buffer));
+//          n = read(newsockfd,buffer,255);
+//          if (n < 0) error("ERROR reading from socket");
+//          printf("Here is the message: %s\n",buffer);
+//          n = write(newsockfd,"I got your message",20);
+//          if (n < 0) error("ERROR writing to socket");
+//          close(newsockfd);
+//          close(sockfd);
+//          count = count + 1;
+//      }
+//     //  listen(sockfd,5);
+//     //  clilen = sizeof(cli_addr);
+//     //  newsockfd = accept(sockfd, 
+//     //              (struct sockaddr *) &cli_addr, 
+//     //              &clilen);
+//     //  if (newsockfd < 0) 
+//     //       error("ERROR on accept");
+//     //  bzero(buffer,256);
+//     //  n = read(newsockfd,buffer,255);
+//     //  if (n < 0) error("ERROR reading from socket");
+//     //  printf("Here is the message: %s\n",buffer);
+//     //  n = write(newsockfd,"I got your message",18);
+//     //  if (n < 0) error("ERROR writing to socket");
+//      return 0; 
+// }
+
+
+// For finding bandwidth
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno;
      socklen_t clilen;
-     char buffer[256];
+     char buffer[1401];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
+
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     int opt = 1;
+     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+         perror("setsockopt");
+         exit(EXIT_FAILURE);
+     }        
+     if (sockfd < 0) 
+         error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+         sizeof(serv_addr)) < 0) 
+         error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+         (struct sockaddr *) &cli_addr, 
+         &clilen);
+     if (newsockfd < 0) 
+         error("ERROR on accept");
      int count = 1;
      while (count <= 1000) {
-         sockfd = socket(AF_INET, SOCK_STREAM, 0);
-         if (sockfd < 0) 
-             error("ERROR opening socket");
-         bzero((char *) &serv_addr, sizeof(serv_addr));
-         portno = atoi(argv[1]);
-         serv_addr.sin_family = AF_INET;
-         serv_addr.sin_addr.s_addr = INADDR_ANY;
-         serv_addr.sin_port = htons(portno);
-         if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                  sizeof(serv_addr)) < 0) 
-                  error("ERROR on binding");
-         listen(sockfd,5);
-         clilen = sizeof(cli_addr);
-         newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-         if (newsockfd < 0) 
-              error("ERROR on accept");
-         bzero(buffer,256);
-         n = read(newsockfd,buffer,255);
+         memset(buffer, 0, sizeof(buffer));
+         n = read(newsockfd,buffer, sizeof(buffer));
          if (n < 0) error("ERROR reading from socket");
-         printf("Here is the message: %s\n",buffer);
-         n = write(newsockfd,"I got your message",19);
+         memset(buffer, 'a', sizeof(buffer) - 1);
+         buffer[sizeof(buffer) - 1] = '\0';
+        printf("msg received");
+         n = write(newsockfd, buffer, sizeof(buffer));
          if (n < 0) error("ERROR writing to socket");
-         close(newsockfd);
-         close(sockfd);
-         sleep(60);
          count = count + 1;
      }
-    //  listen(sockfd,5);
-    //  clilen = sizeof(cli_addr);
-    //  newsockfd = accept(sockfd, 
-    //              (struct sockaddr *) &cli_addr, 
-    //              &clilen);
-    //  if (newsockfd < 0) 
-    //       error("ERROR on accept");
-    //  bzero(buffer,256);
-    //  n = read(newsockfd,buffer,255);
-    //  if (n < 0) error("ERROR reading from socket");
-    //  printf("Here is the message: %s\n",buffer);
-    //  n = write(newsockfd,"I got your message",18);
-    //  if (n < 0) error("ERROR writing to socket");
+     close(newsockfd);
+     close(sockfd);
      return 0; 
 }
