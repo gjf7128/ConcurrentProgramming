9f6ea60451ee537822c228221d8dc95330d1a87a
gjf7128 <gjf7128@rit.edu>
2024-05-06 12:09:22 -0400

    Fixed looping for input issue.

--- FinalProject/src/MainProject.java
+++ FinalProject/src/MainProject.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class MainProject {
     public static void main(String[] args) {
+        World world = new World();
 
+        Scanner scanner = new Scanner(System.in);
         while(true) {
-            Scanner scanner = new Scanner(System.in);
             // May change this to just list options
             System.out.println("What would you like to do?");
             String command = scanner.nextLine();
@@ -14,11 +15,16 @@ public class MainProject {
             else if (command.equals("use")) {
                 //interact with nodes of any kind
             }
+            else if (command.equals("show all")) {
+                world.showBuildings();
+                world.showWorkers();
+                world.showNodes();
+            }
             else if (command.equals("quit")) {
                 //break out of the loop
                 break;
             }
-            scanner.close();
         }
+        scanner.close();
     }
 }
--- FinalProject/src/World.java
+++ FinalProject/src/World.java
@@ -4,10 +4,13 @@ import java.util.List;
 public class World {
 
     private List<Node> resourceNodes;
+    private List<Worker> workers;
+
+    private List<Node> buildingNodes;
 
     public World() {
         resourceNodes = new ArrayList<>();
-        List<Node> nodes = List.of(new Node(NodeType.TREE, 2, 20),
+        List<Node> nodesToAdd = List.of(new Node(NodeType.TREE, 2, 20),
                 new Node(NodeType.TREE, 2, 20),
                 new Node(NodeType.TREE, 2, 20),
                 new Node(NodeType.TREE, 2, 20),
@@ -17,7 +20,15 @@ public class World {
                 new Node(NodeType.ANIMAL, 2, 20),
                 new Node(NodeType.ANIMAL, 2, 20),
                 new Node(NodeType.ANIMAL, 2, 20));
-        resourceNodes.addAll(nodes);
+        resourceNodes.addAll(nodesToAdd);
+
+        workers = new ArrayList<>();
+        List<Worker> workersToAdd = List.of(new Worker(), new Worker());
+        workers.addAll(workersToAdd);
+
+        buildingNodes = new ArrayList<>();
+        List<Node> buildingsToAdd = List.of(new Node(NodeType.FRIENDLY_BUILDING, 1, 100));
+        buildingNodes.addAll(buildingsToAdd);
     }
 
     public Node selectNode(int index) {
@@ -26,7 +37,27 @@ public class World {
 
     public void showNodes() {
         for (Node node : resourceNodes) {
-            System.out.println(node);
+            System.out.println(node + "\n");
+        }
+    }
+
+    public Worker selectWorker(int index) {
+        return this.workers.get(index);
+    }
+
+    public void showWorkers() {
+        for (Worker worker : workers) {
+            System.out.println(worker + "\n");
+        }
+    }
+
+    public Node selectBuilding(int index) {
+        return this.buildingNodes.get(index);
+    }
+
+    public void showBuildings() {
+        for (Node building : buildingNodes) {
+            System.out.println(building + "\n");
         }
     }
 }
